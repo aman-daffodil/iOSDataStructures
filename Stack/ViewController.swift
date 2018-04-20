e1d883191b0af870471adcc69aacb94652ffb195
aman gupta <aman.gupta@daffodilsw.com>
2018-04-20 13:12:28 +0530

    Initial Commit

---
+++ Stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stack
+//
+//  Created by Daffolapmac on 20/04/18.
+//  Copyright © 2018 Daffolapmac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
